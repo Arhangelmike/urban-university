cdeb27d09157429d2cd69127331fe115caa8f58e
Arhangelmike <1wrag1@gmail.com>
2024-05-27 20:53:21 +0300

    work

--- Lessons/2.py
+++ Lessons/2.py
@@ -1,3 +1,4 @@
+# исходные данные
 data_structure = [
     [1, 2, 3],
     {'a': 4, 'b': 5},
@@ -5,20 +6,8 @@ data_structure = [
     "Hello",
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-
-
-def con_dict_to_list(from_dict):
-    con_lis_keys = list(from_dict.keys())
-    con_lis_value = list(from_dict.values())
-    from_dict = con_lis_value + con_lis_keys
-    return (from_dict)
-
-
-from_dict = data_structure
-print(from_dict)
-
-
-for j in range(11):
+# функция однократного прогона листа через распаковку содержимого внутренних листов\сетов\словарей\туплов
+def raspskovka(from_dict):
     for i in from_dict:
         if isinstance(i, list):
             temp_list1 = list(i)
@@ -33,30 +22,35 @@ for j in range(11):
             from_dict.remove(i)
             from_dict.append(temp_list3)
         elif isinstance(i, dict):
-            temp_list = con_dict_to_list(i)
+            con_lis_keys = list(i.keys())
+            con_lis_value = list(i.values())
+            temp_list = con_lis_value + con_lis_keys
             from_dict.remove(i)
             from_dict.append(temp_list)
-    print(from_dict)
+    return(from_dict)
 
+#
+from_dict = data_structure
+print(from_dict)
+TYPES = [int, str]
+# обращение к функции столько раз cколько элементов листа не относящихся к типам: int и  str
+
+while any(type(x) not in TYPES for x in from_dict):
+    from_dict = raspskovka(from_dict)
+
+print(from_dict)
+
+# считаем количество чисел и количество символов в строках
 num_sum = 0
 str_len = ''
-
 for i in from_dict:
     if isinstance(i, int):
         num_sum = num_sum + i
     elif isinstance(i, str):
         str_len = str_len + i
-        result = ''.join([k for k in str_len if not k.isdigit()])
-        # Забрать все буквы в строку
-        result2 = ''.join([k for k in str_len if k.isdigit()])
-        # Забрать число из строки !! проверить если их несколько то сложить дописать код
-        #print(str_len)
-        #print(result)
-        #print(result2)
-# не пододбрал варианта условия (пока в элементах списка не останеться элементы типа int и str делать)
 
-num_sum = num_sum + int(result2)
-str_len2 = len(result)
-#print(str_len2)
-#print(num_sum)
-print(num_sum + str_len2)
+
+print("Все строки: ", len(str_len))
+print("Все числа : ", num_sum)
+print("подсчёта суммы всех чисел и длин всех строк:", num_sum + len(str_len))
+
