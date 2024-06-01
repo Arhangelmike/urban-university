101fa4d1565918dd52f6ccb0520243d750c54515
Arhangelmike <1wrag1@gmail.com>
2024-06-02 00:30:21 +0300

    work

--- Lessons/Name_space_test.py
+++ Lessons/Name_space_test.py
@@ -1,9 +1,19 @@
+#1. Создайте новую функцию def test_function
 def test_function():
-    print('Я в test_function')
+    print('Я в test_function') #промежуточный вывод
+
+    '''2. Создайте другую функцию внутри функции inner_function,
+     функция должна печатать значение "Я в области видимости функции test_function"'''
+
     def inner_function():
         print('Я в области видимости функции test_function')
 
+    # 3. Вызовите функцию inner_function внутри функции test_function
     inner_function()
 
-
+#4. Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы
+#OUTPUT^
+#NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
+#
+#inner_function()
 test_function()
