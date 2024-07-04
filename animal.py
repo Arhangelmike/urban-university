58730fa16b08addd2e8d01131e26d54db6ef3d55
Arhangelmike <1wrag1@gmail.com>
2024-07-05 00:47:21 +0300

    work

--- animal.py
+++ animal.py
@@ -6,34 +6,36 @@ class animal:
 
 class predator(animal):
     def __init__(self, name):
-        self.name = name
+        self.name = name # переопределим имя класса
 
 
     def eat(self, food):
         self.food = food
         if food == True:
             animal.fed = True
-            print(f'{self.name}  съел {food.name}')
+            animal.alive = True
+            print(f'{self.name}  съел {food.name}')# !!! predator - alive {animal.alive} fed {animal.fed}
         else:
+            animal.fed = False
             animal.alive = False
-            print(f'{self.name}  не стал есть {food.name}')
-
-
+            print(f'{self.name}  не стал есть {food.name} ')
 
 
 class mammal(animal):
 
     def __init__(self, name):
-        self.name = name
+        self.name = name  # переопределим имя класса
 
     def eat(self, food):
         self.food = food
-        if food == True:
+        if food != True:
             animal.fed = True
-            print(f'{self.name}  съел  {food.name}')
+            animal.alive = True
+            print(f'{self.name}  съел {food.name}')
         else:
+            animal.fed = False
             animal.alive = False
-            print(f'{self.name} не стал есть  {food.name}')
+            print(f'{self.name}  не стал есть   {food.name}') #!!! mammal - alive {animal.alive} fed {animal.fed}
 
 
 class plant:
@@ -44,7 +46,7 @@ class plant:
 class flower(plant):
     def __init__(self, name):
         self.name = name
-
+        plant.edible = False # для контроля того что значение верное уйдет а не дефолтное от  родительского класса
 
 
 class fruit(plant):
@@ -53,17 +55,17 @@ class fruit(plant):
         plant.edible = True
 
 
-a1 = predator('Волк с Уолл-Стрит')
-a2 = mammal('Хатико')
-p1 = flower('Цветик семицветик')
-p2 = fruit('Заводной апельсин')
+a1 = predator('Волк с Уолл-Стрит') # вызов класса хищник и передача в него имени
+a2 = mammal('Хатико') # вызов класса млекопитающие и передача в него имени
+p1 = flower('Цветик семицветик')# вызов класса цветы и передача в него имени
+p2 = fruit('Заводной апельсин')# вызов класса фрукты и передача в него имени
 
-print(a1.name)
-print(p1.name)
+print(a1.name)# распечатать имя переданное в класс
+print(p1.name)# распечатать имя переданное в класс
 
-print(a1.alive)
-print(a2.fed)
-a1.eat(p1)
-a2.eat(p2)
-print(a1.alive)
-print(a2.fed)
+print(a1.alive)# распечатать статус хищника жив
+print(a2.fed)# распечатать статус млекопитающего накормлен
+a1.eat(p1)# передать цветок в метод "кушать" хищника должен стать alive = False \ fed = False
+a2.eat(p2)# передать фрукт в метод класса "кушать" млекопитающего должен стать alive = True \ fed = True
+print(a1.alive)# распечатать статус хищника жив
+print(a2.fed)# распечатать статус млекопитающего накормлен
