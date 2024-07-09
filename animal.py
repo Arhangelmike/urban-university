9a7988af3630792c70933ca973bfec39c71056ae
Arhangelmike <1wrag1@gmail.com>
2024-07-10 01:00:24 +0300

    work

--- animal.py
+++ animal.py
@@ -1,77 +1,72 @@
-class animal:
+class Animal:
     alive = True
     fed = False
     name = ''
 
 
-class predator(animal):
+class Predator(Animal):
     def __init__(self, name):
-        self.name = name # переопределим имя класса
-        animal.__init__(self)
-
+        self.name = name
+        Animal.__init__(self)
 
     def eat(self, food):
-
-
-        if food.edible == True:
-            self.fed = True
-            self.alive = True
-            print(f'{self.name}  съел {food.name}')# !!! predator - alive {animal.alive} fed {animal.fed}
-        else:
-            self.fed = False
-            self.alive = False
-            print(f'{self.name}  не стал есть {food.name} ')
-
-
-class mammal(animal):
-
-    def __init__(self, name):
-        self.name = name  # переопределим имя класса
-        animal.__init__(self)
-
-    def eat(self, food):
-
-
-        if plant.edible != True:
+        if food.edible is True:
             self.fed = True
             self.alive = True
             print(f'{self.name}  съел {food.name}')
         else:
             self.fed = False
             self.alive = False
-            print(f'{self.name}  не стал есть   {food.name}') #!!! mammal - alive {animal.alive} fed {animal.fed}
+            print(f'{self.name}  не стал есть {food.name} ')
 
 
-class plant:
+class Mammal(Animal):
+
+    def __init__(self, name):
+        self.name = name
+        Animal.__init__(self)
+
+    def eat(self, food):
+        if Plant.edible is not True:
+            self.fed = True
+            self.alive = True
+            print(f'{self.name}  съел {food.name}')
+        else:
+            self.fed = False
+            self.alive = False
+            print(f'{self.name}  не стал есть   {food.name}')
+
+
+class Plant:
     name = ''
     edible = False
 
 
-class flower(plant):
+class Flower(Plant):
     def __init__(self, name):
         self.name = name
-        self.edible = False # для контроля того что значение верное уйдет а не дефолтное от  родительского класса
-        plant.__init__(self)
+        self.edible = False  #
+        Plant.__init__(self)
 
 
-class fruit(plant):
+class Fruit(Plant):
     def __init__(self, name):
         self.name = name
         self.edible = True
-        plant.__init__(self)
+        Plant.__init__(self)
 
 
-a1 = predator('Волк с Уолл-Стрит') # вызов класса хищник и передача в него имени
-a2 = mammal('Хатико') # вызов класса млекопитающие и передача в него имени
-p1 = flower('Цветик семицветик')# вызов класса цветы и передача в него имени
-p2 = fruit('Заводной апельсин')# вызов класса фрукты и передача в него имени
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
 
-print(a1.name)# распечатать имя переданное в класс
-print(p1.name)# распечатать имя переданное в класс
+print(a1.name)
+print(p1.name)
 
-print(a1.alive)# распечатать статус хищника жив
-print(a2.fed)# распечатать статус млекопитающего накормлен
-a1.eat(p1)#
-a2.eat(p2) #
-print(a1.alive)# распечатать статус хищника жив
-print(a2.fed)# распечатать статус млекопитающего накормлен
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
