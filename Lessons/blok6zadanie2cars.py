eb14d4e2b78654be7b5f0c7616499272a8813efb
Arhangelmike <1wrag1@gmail.com>
2024-07-19 00:57:29 +0300

    work

--- Lessons/blok6zadanie2cars.py
+++ Lessons/blok6zadanie2cars.py
@@ -6,27 +6,6 @@ class Venicle:
     __COLOR_VARIANTS = ['black', 'grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'light_grey', 'dark_grey',
                         'light_red', 'light_green', 'light_yellow', 'light_blue', 'light_magenta', 'light_cyan',
                         'white']
-'''Атрибут owner(str) - владелец транспорта. (владелец может меняться)
-   Атрибут __model(str) - модель (марка) транспорта. (мы не можем менять название модели)
-   Атрибут __engine_power(int) - мощность двигателя. (мы не можем менять мощность двигателя самостоятельно)
-   Атрибут __color(str) - название цвета. (мы не можем менять цвет автомобиля своими руками)
-         А так же атрибут класса:
-   Атрибут класса __COLOR_VARIANTS, в который записан список допустимых цветов для окрашивания. (Цвета написать свои)'''
-
-class Sedan(Venicle):
-    def __init__(self):
-        __PASSENGERS_LIMIT = 5
-
-'''Метод get_model - возвращает строку: "Модель: <название модели транспорта>"
-Метод get_horsepower - возвращает строку: "Мощность двигателя: <мощность>"
-Метод get_color - возвращает строку: "Цвет: <цвет транспорта>"
-Метод print_info - распечатывает результаты методов (в том же порядке): 
-                   get_model, get_horsepower, 
-                   get_color; а так же владельца в конце в формате "Владелец: <имя>"
-Метод set_color - принимает аргумент new_color(str),
-                  меняет цвет __color на new_color, 
-                  если он есть в списке __COLOR_VARIANTS, 
-                  в противном случае выводит на экран надпись: "Нельзя сменить цвет на <новый цвет>".'''
 
     def get_model(self):
         print(f'Модель: {Venicle.__model}')
@@ -39,15 +18,21 @@ class Sedan(Venicle):
     def get_color(self):
         print(f'Цвет: {Venicle.__color}')
 
+
     def print_info(self):
         print(f'  {self.get_model}, {self.get_horsepower}, {self.get_color} ,  {self.owner}')
 
+
     def set_color(self, new_color):
         self.new_color = new_color
-        is new_color in __COLOR_VARIANTS:
+        if new_color in Venicle.__COLOR_VARIANTS:
             print(f'{new_color}')
-        else
+        else:
             print(f'Нельзя сменить цвет на {new_color}')
+
+
+class Sedan(Venicle):
+        __PASSENGERS_LIMIT = 5
 
 
 
