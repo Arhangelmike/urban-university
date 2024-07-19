9c4ec1d1c470e3a235c1395099cd83f57023ab53
Arhangelmike <1wrag1@gmail.com>
2024-07-20 00:51:43 +0300

    work

--- Lessons/blok6zadanie2cars.py
+++ Lessons/blok6zadanie2cars.py
@@ -1,26 +1,30 @@
 class Venicle:
-    owner=''
-    __model=''
-    __engine_power=0
-    __color=''
+    owner: str
+    __model: str
+    __engine_power: int
+    __color: str
     __COLOR_VARIANTS = ['black', 'grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'light_grey', 'dark_grey',
                         'light_red', 'light_green', 'light_yellow', 'light_blue', 'light_magenta', 'light_cyan',
                         'white']
 
     def get_model(self):
-        print(f'Модель: {Venicle.__model}')
+        print(f'Модель: {self.__model}')
 
 
     def get_horsepower(self):
-        print(f'Мощность двигателя:  {Venicle.__engine_power}')
+        print(f'Мощность двигателя:  {self.__engine_power}')
 
 
     def get_color(self):
-        print(f'Цвет: {Venicle.__color}')
+        print(f'Цвет: {self.__color}')
 
 
     def print_info(self):
-        print(f'  {self.get_model}, {self.get_horsepower}, {self.get_color} ,  {self.owner}')
+        print(f'{self.owner}')
+        print(self.get_model)
+        print(self.get_horsepower)
+        print(self.get_color)
+
 
 
     def set_color(self, new_color):
@@ -32,20 +36,22 @@ class Venicle:
 
 
 class Sedan(Venicle):
-        __PASSENGERS_LIMIT = 5
+    __PASSENGERS_LIMIT = 5
+    def __init__(self, *args):
+        self.owner =
+        # print(*args)
 
 
-
-# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+#  Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 
-# Изначальные свойства
+# # Изначальные свойства
 vehicle1.print_info()
-
-# Меняем свойства (в т.ч. вызывая методы)
-vehicle1.set_color('Pink')
-vehicle1.set_color('BLACK')
-vehicle1.owner = 'Vasyok'
-
-# Проверяем что поменялось
-vehicle1.print_info()
+#
+# # Меняем свойства (в т.ч. вызывая методы)
+# vehicle1.set_color('Pink')
+# vehicle1.set_color('BLACK')
+# vehicle1.owner = 'Vasyok'
+#
+# # Проверяем что поменялось
+# vehicle1.print_info()
