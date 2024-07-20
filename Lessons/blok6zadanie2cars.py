45b2efb5d63dffa82a008d8b55e214ae86548e21
Arhangelmike <1wrag1@gmail.com>
2024-07-20 11:57:10 +0300

    work

--- Lessons/blok6zadanie2cars.py
+++ Lessons/blok6zadanie2cars.py
@@ -9,24 +9,25 @@ class Venicle:
         self.__color = color
 
 
-
     def get_model(self):
-        print(f'Модель: {self.__model}')
+        # print(f'Модель: {self.__model}')
+        return f'Модель: {self.__model}'
 
 
     def get_horsepower(self):
-        print(f'Мощность двигателя:  {self.__engine_power}')
+        # print(f'Мощность двигателя:  {self.__engine_power}')
+        return f'Мощность двигателя: {self.__engine_power}'
 
 
     def get_color(self):
-        print(f'Цвет: {self.__color}')
-
+        # print(f'Цвет: {self.__color}')
+        return f'Цвет: {self.__color}'
 
     def print_info(self):
-        print(self.owner)
-        print(self.get_model)
-        print(self.get_horsepower)
-        print(self.get_color)
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f'Владелец: {self.owner}')
 
 
 
@@ -40,8 +41,8 @@ class Venicle:
 
 class Sedan(Venicle):
     __PASSENGERS_LIMIT = 5
-    def __init__(self, *args):
-        pass
+    # def __init__(self, *args):
+        # pass
         # print(*args)
 
 
@@ -52,9 +53,9 @@ vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 vehicle1.print_info()
 #
 # # Меняем свойства (в т.ч. вызывая методы)
-# vehicle1.set_color('Pink')
-# vehicle1.set_color('BLACK')
-# vehicle1.owner = 'Vasyok'
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
 #
 # # Проверяем что поменялось
-# vehicle1.print_info()
+vehicle1.print_info()
