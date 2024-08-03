065cff70a2e7a6fcc77e426ceacc798c6aa3bec5
Arhangelmike <1wrag1@gmail.com>
2024-08-04 02:18:16 +0300

    work

--- Lessons/block6zadanieDopFugere.py
+++ Lessons/block6zadanieDopFugere.py
@@ -4,55 +4,51 @@ class Figure:
 
     def __init__(self, color=(0, 0, 0), *sides):
         self.__color = list(color)
-        self.__sides = list(sides) if self.__is_valid_sides(*sides) else [1] * self.sides_countt
+        self.__sides = list(sides) if self.__is_valid_sides(*sides) else [1] * self.sides_count
         self.filled = False
 
 
     def get_color(self):
         return self.__color
-    '''возврат три числа каждое в диапазоне от 0 до 255'''
 
 
     def __is_valid_color(self, R, G, B):
         return all(isinstance(colors1, int) and 0 <= colors1 <= 255 for colors1 in (R, G, B))
 
+
     def set_color(self, R, G, B):
         if self.__is_valid_color(R, G, B):
             self.__color = (R, G, B)
 
-    def __is_valid_sides(self, *args):
-        if  all(isinstance(args1, int) and args1 > 0 for args1 in args):
-            is_valid = len(args)
-        return is_valid
-
-    '''принимает неограниченное кол-во сторон, 
-    возвращает True если все стороны целые положительные числа и
-     кол-во новых сторон совпадает с текущим, False - во всех остальных случаях.'''
 
     def get_sides(self):
         return self.__sides
 
-    def __len__(self):
-        return sum(self.__sides) #  возвращать периметр фигуры.
+
+    def __is_valid_sides(self, *args):
+        return all(isinstance(args1, int) and args1 > 0 for args1 in args)
+
 
     def set_sides(self, *new_sides):
         if self.__is_valid_sides(*new_sides):
             self.__sides = list(new_sides)
 
 
+    def __len__(self):
+        return sum(self.__sides) #  возвращать периметр фигуры.
+
 class Circle(Figure):
     sides_count = 1
 
     def __init__(self, color, *sides):
         super().__init__(color, *sides)
-        self.__radius = self._Figure__sides[0]
+        #self.__radius = self._Figure__sides[0]
 
     def get_square(self):
         Pi = math.pi
-        __radius = self.__sides/(2*Pi)
-        return Pi*__radius*__radius
-    '''Атрибут __radius, рассчитать исходя из длины окружности (одной единственной стороны).
-Метод get_square возвращает площадь круга (можно рассчитать как через длину, так и через радиус).'''
+        self.__radius = self.__sides/(2*Pi)
+        R = self.__radius
+        return Pi * R * R
 
 
 class Triangle(Figure):
@@ -77,6 +73,8 @@ class Cube(Figure):
 
     def get_volume(self):
         return self.get_sides()[0] ** 3
+
+
 
     '''Переопределить __sides сделав список из 12 одинаковы сторон (передаётся 1 сторона)
 Метод get_volume, возвращает объём куба.'''
