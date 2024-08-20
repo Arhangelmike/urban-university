82949d264fc05152d08ce130b5ca46ab8ee48805
Arhangelmike <1wrag1@gmail.com>
2024-08-20 12:23:13 +0300

    work

---
+++ Lessons/block7z2.py
@@ -0,0 +1,20 @@
+def custom_write(file_name: str, strings: list):
+    strings_dict={}
+    file = open(file_name, 'w+', encoding='utf-8')
+    for i in strings:
+        strings_dict[(strings.index(i) + 1, file.tell())] = i
+        file.write(f'{i}\n')
+    file.close()
+    return strings_dict
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+pos_and_strings = custom_write('C:/Users/user/Pictures/1/file_name1.txt', info)
+for elem in pos_and_strings.items():
+    print(elem)
---
+++ Lessons/testcreatefile.py
@@ -0,0 +1,3 @@
+file = open('C:/Users/user/Pictures/1/000005.txt', 'w+')
+file.write(f'\n1')
+file.close()
