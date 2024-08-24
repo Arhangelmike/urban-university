a4e778cb485d00595d23a505b0214b4ab8546b67
Arhangelmike <1wrag1@gmail.com>
2024-08-24 10:22:36 +0300

    work

--- Lessons/block7z3.py
+++ Lessons/block7z3.py
@@ -1,3 +1,4 @@
+import os
 class WordsFinder:
     def __init__(self, *file_names):
         self.file_names = file_names
@@ -35,9 +36,16 @@ class WordsFinder:
                     count += 1
             find_word_quant[name] = count
         return find_word_quant
-
-
-finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words()) # Все слова
-print(finder2.find('TEXT')) # 3 слово по счёту
-print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+# положить файлы в паку с путем ниже
+path = 'C:/Users/user/Downloads/All1/All/'
+f = [f'{path}{file}' for file in os.listdir(path)]
+# print(f) # проверка на то что получили список файлов
+for filename in f:
+    # print(f'{filename}') # check that IN one filename
+    finder2 = WordsFinder(f'{filename}')
+    print(finder2.get_all_words()) # Все слова
+    print(finder2.find('TEXT')) # 3 слово по счёту
+    print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+    # раскоментировать для другого поискового слова из проверки
+    # print(finder2.find('The'))
+    # print(finder2.count('the'))
