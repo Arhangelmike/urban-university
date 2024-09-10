12a2e1c39559a8b7832c1a918b6fb3a15ec7ad07
Arhangelmike <1wrag1@gmail.com>
2024-09-11 01:11:35 +0300

    work

---
+++ Lessons/block10z1.py
@@ -0,0 +1,43 @@
+from time import sleep
+from threading import Thread
+from datetime import datetime
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i in range(1, word_count + 1):
+            word = f"Какое-то слово № {i}"
+            file.write(word + '\n')
+
+            sleep(0.1)
+
+    print(f"Завершилась запись в файл {file_name}")
+
+# method start 1
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+
+# method start 2
+
+start_time_thread = datetime.now()
+
+thread1 = Thread(target=write_words, args=(10, 'example5.txt'))
+thread2 = Thread(target=write_words, args=(30, 'example6.txt'))
+thread3 = Thread(target=write_words, args=(200, 'example7.txt'))
+thread4 = Thread(target=write_words, args=(100, 'example8.txt'))
+
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
+
+end_time_thread = datetime.now()
+delta_time = end_time_thread - start_time_thread
+print(f"Время выполнения потоков: {delta_time} секунд")
--- Lessons/testcreatefile.py
+++ Lessons/testcreatefile.py
@@ -1,3 +1,3 @@
-file = open('C:/Users/user/Pictures/1/00000444.txt', 'w+')
+file = open('C:/Users/user/Pictures/1/000004445.txt', 'w+')
 file.write(f'\n1')
 file.close()
