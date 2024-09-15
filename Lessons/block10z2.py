7adf9686dc6e889e13e07c629d26168f70ea6fa6
Arhangelmike <1wrag1@gmail.com>
2024-09-16 00:32:41 +0300

    work

--- Lessons/block10z2.py
+++ Lessons/block10z2.py
@@ -1,28 +1,36 @@
-import threading
+from threading import Thread
+import time
 
-counter = 0
-known_enemy = 100
-class Knight(threading.Thread):
+class Knight(Thread):
     def __init__(self, name, power):
-        threading.Thread.__init__(self)
+        super().__init__()
         self.name = name
         self.power = power
+        self.counter = 0
+        self.known_enemy = 100
         print(f'{name}, на нас напали!')
-        self.run(name, power)
 
 
-    def run(self, name, power):
-        global known_enemy, counter
-        while known_enemy > 0:
-            counter += 1
-            known_enemy = known_enemy - self.power
-            print(f'{self.name} сражается {counter} дней..., осталось {known_enemy} врагов.')
-        print(f'{self.name} одержал победу спустя {counter} дней(дня)!')
-        counter = 0
-        known_enemy = 100
+
+    def run(self):
+        while self.known_enemy > 0:
+            self.counter += 1
+            self.known_enemy = self.known_enemy - self.power
+            time.sleep(1)
+            print(f'{self.name} сражается {self.counter} дней..., осталось {self.known_enemy} врагов.')
+        print(f'{self.name} одержал победу спустя {self.counter} дней(дня)!')
 
 
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
 
-print("The end")
+
+first_knight.start()
+second_knight.start()
+
+
+first_knight.join()
+second_knight.join()
+
+
+print('Все бои окончены!')
