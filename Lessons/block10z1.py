bca3d41a4a94c992365b842187f4c68a9f196cc4
Arhangelmike <1wrag1@gmail.com>
2024-09-15 16:57:12 +0300

    work

--- Lessons/block10z1.py
+++ Lessons/block10z1.py
@@ -12,15 +12,19 @@ def write_words(word_count, file_name):
     print(f"Завершилась запись в файл {file_name}")
 
 # method start 1
+
+start_time_thread = datetime.now()
 write_words(10, 'example1.txt')
 write_words(30, 'example2.txt')
 write_words(200, 'example3.txt')
 write_words(100, 'example4.txt')
-
+end_time_thread = datetime.now()
+delta_time = end_time_thread - start_time_thread
+print(f"Время выполнения потоков: {delta_time} секунд")
 
 # method start 2
 
-start_time_thread = datetime.now()
+start_time = datetime.now()
 
 thread1 = threading.Thread(target=write_words, args=(10, 'example5.txt'))
 thread2 = threading.Thread(target=write_words, args=(30, 'example6.txt'))
@@ -43,6 +47,6 @@ thread2.join()
 thread3.join()
 thread4.join()
 
-end_time_thread = datetime.now()
-delta_time = end_time_thread - start_time_thread
-print(f"Время выполнения потоков: {delta_time} секунд")
+end_time = datetime.now()
+delta_time1 = end_time - start_time
+print(f"Время выполнения потоков: {delta_time1} секунд")
