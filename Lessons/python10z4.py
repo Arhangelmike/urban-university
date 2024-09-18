32bfbf6d0f968cfcfac8bfb46ba3f85e248a97d6
Arhangelmike <1wrag1@gmail.com>
2024-09-18 17:50:40 +0300

    work

--- Lessons/python10z4.py
+++ Lessons/python10z4.py
@@ -4,9 +4,12 @@ from datetime import datetime
 
 
 class Table:
-    def __init__(self, number):
-        self.number  = number
-        self.guest = None
+
+    def __init__(self, number, guest=None):
+        self.number = number
+        self.guest = guest
+
+
 
 class Guest(threading.Thread):
 
@@ -17,20 +20,36 @@ class Guest(threading.Thread):
     def run(self):
         self.waiting = random.randint(3,10)
         time.sleep(self.waiting)
+
+
 class Cafe:
 
     def __init__(self, *tables):
-        self.tables = list(tables)
         self.queue = Queue()
-
-
+        self.tables = list(tables)
 
     def guest_arrival(self, *guests):
-        self.guests = list(guests)
+        for guest in guests:
+            for table in self.tables:
+                if table.guest is None:
+                    table.guest = guest
+                    guest.start()
+                    print(f"{guest.name} сел(-а) за стол номер {table.number}")
+                    break
+                else:
+                    self.queue.put(guest)
+                    print(f"{guest.name} в очереди")
 
-
-    def discuss_guests(self, *args):
-        pass
+    def discuss_guests(self):
+        while not self.queue.empty() or any([table.guest for table in self.tables]):
+            for table in self.tables:
+                if table.guest is not None and not table.guest.is_alive():
+                    print(f'{table.guest.name} покушал(-а) и ушёл(ушла)')
+                    print(f'Стол номер {table.number} свободен')
+                    table.guest = None
+                if not self.queue.empty() and table.guest is None:
+                    table.guest = self.queue.get()
+                    print(f'{table.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
 
 
 # Создание столов
