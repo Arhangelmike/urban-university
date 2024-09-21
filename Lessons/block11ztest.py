88ddcedca3ca3993bea15ccd7a652b261f15758f
Arhangelmike <1wrag1@gmail.com>
2024-09-21 22:40:05 +0300

    work

--- Lessons/block11ztest.py
+++ Lessons/block11ztest.py
@@ -32,32 +32,45 @@
 #
 #     for content in contents:
 #         print(content)
+#
+#
+# import logging
+# import multiprocessing
+# from multiprocessing import Process, Lock
+#
+#
+# def printer(item, lock):
+#     """
+#     Выводим то что передали
+#     """
+#     lock.acquire()
+#     try:
+#         print(item)
+#     finally:
+#         lock.release()
+#
+#
+# if __name__ == '__main__':
+#     lock = Lock()
+#     items = ['tango', 'foxtrot', 10]
+#     multiprocessing.log_to_stderr()
+#
+#     logger = multiprocessing.get_logger()
+#     logger.setLevel(logging.INFO)
+#
+#     for item in items:
+#         p = Process(target=printer, args=(item, lock))
+#         p.start()
 
-
-import logging
-import multiprocessing
-from multiprocessing import Process, Lock
-
-
-def printer(item, lock):
-    """
-    Выводим то что передали
-    """
-    lock.acquire()
-    try:
-        print(item)
-    finally:
-        lock.release()
-
-
-if __name__ == '__main__':
-    lock = Lock()
-    items = ['tango', 'foxtrot', 10]
-    multiprocessing.log_to_stderr()
-
-    logger = multiprocessing.get_logger()
-    logger.setLevel(logging.INFO)
-
-    for item in items:
-        p = Process(target=printer, args=(item, lock))
-        p.start()
+#
+# from multiprocessing import Pool
+#
+#
+# def doubler(number):
+#     return number * 2
+#
+#
+# if __name__ == '__main__':
+#     numbers = [5, 10, 20]
+#     pool = Pool(processes=3)
+#     print(pool.map(doubler, numbers))
