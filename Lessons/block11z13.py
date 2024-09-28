7acde36611755f9d8feca04cc587df4a94d06d79
Arhangelmike <1wrag1@gmail.com>
2024-09-28 23:47:56 +0300

    work

--- Lessons/block11z13.py
+++ Lessons/block11z13.py
@@ -1,28 +1,24 @@
+import inspect, sys
 def introspection(obj):
+
     obj_type = type(obj).__name__
-
-    attributes = [attribute for attribute in dir(obj)
-                  if not callable(getattr(obj, attribute))]
-
-    methods = [method for method in attributes if callable(getattr(obj, method))]
-
+    # print(obj_type)
+    attributes = [attribute for attribute in dir(obj) if not callable(getattr(obj, attribute))]
+    # print(attributes)
+    methods = [method for method in dir(obj) if callable(getattr(obj, method)) and not method.startswith("__")]
+    # print(methods)
     module = obj.__class__.__module__
+    # module = sys.argv[0]
+    # print(f'{module}')
 
     info = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module}
-
     return info
+
 
 number_info = introspection(42)
 print(number_info)
-
 string_info = introspection('Hi all')
 print(string_info)
-
 list_info = introspection(['sting', 4, 7.5])
 print(list_info)
 
-
-for attribute in dir(callable):
-    print(attribute, getattr(callable, attribute))
-
-print('callable:', str(callable.__subclasshook__))
---
+++ Lessons/introspection.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+number_info = introspection(42)
+print(number_info)
+
+string_info = introspection('Hi all')
+print(string_info)
+
+list_info = introspection(['sting', 4, 7.5])
+print(list_info)
