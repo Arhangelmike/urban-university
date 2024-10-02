b0c2eadd3ea8adbe569cbd2d27f8af73daa82552
Arhangelmike <1wrag1@gmail.com>
2024-10-03 00:07:06 +0300

    work

--- Lessons/utest/unittest1.py
+++ Lessons/utest/unittest1.py
@@ -1,13 +1,60 @@
-import runner, unittest
+from unittest import TestCase
+import Runner
+
 class RunnerTest(TestCase):
 
     def test_walk(self):
         walk_test1 = Runner.walk
-        for wt in range(1, 10):
+
+        for wt in range(10):
+            global wt
             wt = walk_test1
-            print(wt)
+
         self.assertEqual(wt.distance, 50)
     def test_run(self):
         pass
     def test_challenge(self):
         pass
+
+
+import unittest
+from unittest import TestCase
+
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(TestCase):
+    def test_walk(self):
+        run_1 = Runner('Tesla')
+        for i in range(10):
+            run_1.walk()
+        self.assertEqual(run_1.distance, 50)
+
+    def test_run(self):
+        run_2 = Runner('Lada')
+        for i in range(10):
+            run_2.run()
+        self.assertEqual(run_2.distance, 100)
+
+    def test_challenge(self):
+        run_3 = Runner('Dodge')
+        run_4 = Runner('Porshe')
+        for i in range(10):
+            run_3.run()
+            run_4.walk()
+        self.assertNotEqual(run_3.distance, run_4.distance)
+
+    is_frozen = False
