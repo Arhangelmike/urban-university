a8d8c2deab249b31c2584a8f6a111d683395830f
Arhangelmike <1wrag1@gmail.com>
2024-10-05 19:07:23 +0300

    work

--- Lessons/utest/unitest_tornament.py
+++ Lessons/utest/unitest_tornament.py
@@ -13,24 +13,22 @@ class TournamentTest(unittest.TestCase):
         self.usein = runner_and_tournament.Runner("Усэйн", 10)
         self.andrey = runner_and_tournament.Runner("Андрей", 9)
         self.nick = runner_and_tournament.Runner("Ник", 3)
+        # self.runners = {'Усэйн': 10, 'Андрей': 9, 'Ник': 3}
+
 
 
     @classmethod
     def tearDownClass(cls):
-        for key, value in cls.all_results.values():
-            print(key, ':', value)
+        for key, value in TournamentTest.all_results.items():
+            print(key, '-:-', value)
 
 
     def test_usein_vs_nick(self):
         a = self.usein
         b = self.nick
         tournament = runner_and_tournament.Tournament(90, a, b)
-        print(tournament)
-        print(a)
-        print(b)
-
         result = tournament.start()
-        print(result)
+        # print(result)
         TournamentTest.all_results[len(TournamentTest.all_results) + 1] = result
         self.assertTrue(result[max(result.keys())] == "Ник")
 
