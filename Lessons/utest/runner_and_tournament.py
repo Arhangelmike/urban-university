3c021f778d64d6a52e0fe415d4af2d7adef07efc
Arhangelmike <1wrag1@gmail.com>
2024-10-07 20:36:19 +0300

    work

--- Lessons/utest/runner_and_tournament.py
+++ Lessons/utest/runner_and_tournament.py
@@ -1,3 +1,43 @@
+# class Runner:
+#     def __init__(self, name, speed=5):
+#         self.name = name
+#         self.distance = 0
+#         self.speed = speed
+#
+#     def run(self):
+#         self.distance += self.speed * 2
+#
+#     def walk(self):
+#         self.distance += self.speed
+#
+#     def __str__(self):
+#         return self.name
+#
+#     def __eq__(self, other):
+#         if isinstance(other, str):
+#             return self.name == other
+#         elif isinstance(other, Runner):
+#             return self.name == other.name
+#
+#
+# class Tournament:
+#     def __init__(self, distance, *participants):
+#         self.full_distance = distance
+#         self.participants = list(participants)
+#
+#     def start(self):
+#         finishers = {}
+#         place = 1
+#         while self.participants:
+#             for participant in self.participants:
+#                 participant.run()
+#                 if participant.distance >= self.full_distance:
+#                     finishers[place] = participant
+#                     place += 1
+#                     self.participants.remove(participant)
+#         return finishers
+#
+#
 class Runner:
     def __init__(self, name, speed=5):
         self.name = name
@@ -11,6 +51,9 @@ class Runner:
         self.distance += self.speed
 
     def __str__(self):
+        return self.name
+
+    def __repr__(self):
         return self.name
 
     def __eq__(self, other):
@@ -35,6 +78,12 @@ class Tournament:
                     finishers[place] = participant
                     place += 1
                     self.participants.remove(participant)
+
         return finishers
 
-
+# first = Runner('Вося', 10)
+# second = Runner('Илья', 5)
+# # third = Runner('Арсен', 10)
+#
+# t = Tournament(101, first, second)
+# print(t.start())
