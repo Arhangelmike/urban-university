de6b6c1b719bbba705bb8aacfba251a003e80563
Arhangelmike <1wrag1@gmail.com>
2024-10-23 22:54:32 +0300

    work

--- bot/db.py
+++ bot/db.py
@@ -12,12 +12,55 @@ block INT
 ''')
 # cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON User1 (email)")
 def add_user(user_id, username, first_name):
+    connection = sqlite3.connect('database_bot.db')
+    cursor = connection.cursor()
     check_user = cursor.execute("SELECT * FROM Users1 WHERE id=?", (user_id,))
 
     if check_user.fetchone() is None:
         cursor.execute(f'''
             INSERT INTO Users1 VALUES('{user_id}', '{username}', '{first_name}', 0)
         ''')
+        connection.commit()
+
+
+def show_users():
+    connection = sqlite3.connect('database_bot.db')
+    cursor = connection.cursor()
+    user_list =  cursor.execute("SELECT * FROM Users")
+    message = ''
+    for user in user_list:
+        message += f"{user[0]} @{user[1]} {user[2]} \n"
+    connection.commit()
+    return message
+
+
+def show_stat():
+    connection = sqlite3.connect('database_bot.db')
+    cursor = connection.cursor()
+    count_users = cursor.execute("SELECT COUNT(*) FROM Users").fetchone()
+    connection.commit()
+    return count_users[0]
+
+def ad_to_block(input_id):
+    connection = sqlite3.connect('database_bot.db')
+    cursor = connection.cursor()
+    cursor.execute(f"UPDATE USERS set block = ? WHERE id = ?",(1, input_id))
+    connection.commit()
+
+
+def check_block(input_id):
+    connection = sqlite3.connect('database_bot.db')
+    cursor = connection.cursor()
+    users = cursor.execute(f"SELECT block FROM Usrs where id = {user_id}").fetchone()
+    connection.commit()
+    return users[0]
+
+def remove_block(input_id):
+    connection = sqlite3.connect('database_bot.db')
+    cursor = connection.cursor()
+    cursor.execute(f"UPDATE USERS set block = ? WHERE id = ?",(0, input_id))
+    connection.commit()
+
 
 connection.commit()
 connection.close()
--- crud_functions.py
+++ crud_functions.py
@@ -13,6 +13,15 @@ def initiate_db():
     price INT NOT NULL
     );
     ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users(
+        id INT PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INT NOT NULL,
+        balance INT NOT NULL
+        );
+        ''')
     connection.commit()
     connection.close()
 
@@ -33,6 +42,14 @@ def add_product(title, description, price):
     connection.commit()
     connection.close()
 
+def add_user(username, email, age, balance):
+    connection = sqlite3.connect('Products.db')
+    cursor = connection.cursor()
+    initiate_db()
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (username, email, age, balance))
+    connection.commit()
+    connection.close()
+
 
 connection.commit()
 connection.close()
@@ -41,3 +58,8 @@ add_product('Product1', '1', 100)
 add_product('Product2', '2', 200)
 add_product('Product3', '3', 300)
 add_product('Product4', '4', 400)
+
+add_user('username1', 'email1@email.ru', 25, 1000)
+add_user('username2', 'email2@email.ru', 35, 1000)
+add_user('username3', 'email3@email.ru', 45, 1000)
+add_user('username4', 'email4@email.ru', 55, 1000)
