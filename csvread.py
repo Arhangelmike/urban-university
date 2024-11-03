efe2d6d7e51b8e6b186344dcfaf92a86eaabbda1
Arhangelmike <1wrag1@gmail.com>
2024-11-04 00:29:31 +0300

    work

--- csvread.py
+++ csvread.py
@@ -7,21 +7,13 @@ import matplotlib.dates as mdates
 
 df = pd.read_csv('csv/data_182.csv', sep=';', parse_dates=['date'], dayfirst=True)
 df['rate'] = round(df['rate'], 2)
-# df['date'] = pd.to_datetime(df['date'])
 df = df.sort_values(by='date', ascending=True)
 unique_codes = df['letter_code'].unique()
 
-# unique_codes = ['USD', 'SEK', 'GBP']
-# fig, ax = plt.subplots()
-# global x, y, labl
-
-plt.figure(figsize=(18, 10))
-for code in unique_codes:
-    currency_data = df[df['letter_code'] == code]
-    plt.plot(currency_data['date'], currency_data['rate'], label=code)
-    # print(currency_data['date'])
-
-
+plt.figure(figsize=(15, 15))
+for code in unique_codes[14:15]:
+        currency_data = df[df['letter_code'] == code]
+        plt.plot(currency_data['date'], currency_data['rate'], label=code)
 
 plt.xlabel('Дата')
 plt.ylabel('Курс')
@@ -32,8 +24,8 @@ plt.gca().xaxis.set_major_locator(mdates.YearLocator())
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
 plt.xticks(rotation=45, ha='right')
 
-# y_ticks = range(int(df['rate'].min()), int(df['rate'].max()) + 1000, 1000)
-# plt.yticks(y_ticks)
+y_ticks = range(int(df['rate'].min()), int(df['rate'].max()) + 1000, 1000)
+plt.yticks(y_ticks)
 
 plt.legend(title='Валюта', bbox_to_anchor=(1.1, 1), loc='upper left', borderaxespad=0.)
 plt.tight_layout()
