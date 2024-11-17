ced47e7ad310d4cbe85a6a0fbeea4b1bef85d380
Arhangelmike <1wrag1@gmail.com>
2024-11-17 11:33:45 +0300

    work

--- csvread3.py
+++ csvread3.py
@@ -1,26 +1,42 @@
 import pandas as pd
-import plotly
-import plotly.graph_objs as go
-import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
-# data = pd.read_csv('csv/data_182small.csv', sep=';', parse_dates=['date'], dayfirst=True)
-# data['rate'] = round(data['rate'], 2)
-# print(unique_codes)
-# df = [go.Scatter(
-#     x=data['date'],
-#     y=data['rate'],
-#     title="setting up colour palette",
-#     color_continuous_scale=["orange", "red", "green", "blue", "purple"])]
-# plotly.offline.plot(df)
-
-data = pd.read_csv('csv/data_182small.csv', encoding = 'UTF-8', sep=';', parse_dates=['date'], dayfirst=True)
+data = pd.read_csv('csv/data_182small.csv', encoding='UTF-8', sep=';', parse_dates=['date'], dayfirst=True)
 data['rate'] = round(data['rate'], 2)
-fig = px.scatter(data,
-    color='letter_code',
-    x=data['date'],
-    y=data['rate'],
-    title="Курс валют",
-    color_continuous_scale=["orange", "red", "green", "blue", "purple"],
-    template = "plotly_dark")
+dfA = pd.DataFrame({
+    "Price": data['rate'],
+    "Years": data['date'],
+    "Currency": data['letter_code']
+})
 
+currencies = dfA['Currency'].unique()
+
+# n_cols = 2
+# n_rows = -(-len(currencies) // n_cols)  # Округление вверх
+# fig = make_subplots(rows=n_rows, cols=n_cols, subplot_titles=currencies)
+fig = go.Figure()
+for idx, currency in enumerate(currencies):
+    # row = idx // n_cols + 1
+    # col = idx % n_cols + 1
+
+    filtered_data = dfA[dfA['Currency'] == currency]
+    fig.add_trace(
+        go.Scatter(x=filtered_data["Years"], y=filtered_data["Price"], name=currency),
+        # row=row,
+        # col=col
+    )
+
+fig.update_layout(
+    # height=400 * n_rows,
+    title="Мультиграфики по валютам",
+    showlegend=False  # Скрыть легенду, если она не нужна
+)
 fig.show()
+# x=data['date']
+# y=data['rate']
+# количество данных по валюте в процентах от частоты упоминания ее в файле
+# fig = px.pie(data, values=data['rate'], names=data['letter_code'], title='Data value')
+# fig = px.scatter(x, y)
+# fig.show()
+
