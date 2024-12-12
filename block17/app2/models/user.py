17d7f93696da9fb8338def94b061150684034b57
Arhangelmike <1wrag1@gmail.com>
2024-12-12 21:16:22 +0300

    work

--- block17/app2/models/user.py
+++ block17/app2/models/user.py
@@ -3,12 +3,8 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.schema import CreateTable
 from block17.app2.backend.db import Base
 from block17.app2.models import Task
-'''Нет в лекции!!!!! 
-при импорте файлов учесть, что корнем является ближайшая папка с виртуальным окружением .venv
-поэтому путь до файла db.py
-не такой    ----    C:\Users\user\PycharmProjects\pythonProject1\block17\app2\backend\db.py
-и не такой   ----    app2.backend.db 
-а выглядит вот так, в моем случае   ------  block17.app2.backend.db'''
+
+
 class User(Base):
     __tablename__ = 'users'
     __table_args__ = {'extend_existing': True}
